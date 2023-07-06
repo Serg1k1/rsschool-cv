8f7ed98f6102bbee30b5e4eba526b8261213a0fb
Sergey <kindratsergij60@gmail.com>
2023-07-06 13:53:31 +0300

    feat: done "index" page

--- js/script.js
+++ js/script.js
@@ -8,4 +8,21 @@ function documentActions(e) {
     if (targetElement.closest('.icon-menu')) {
         document.documentElement.classList.toggle('menu-open');
     }
+
+    if (targetElement.closest('[data-goto]')) {
+        document.documentElement.classList.contains('menu-open') ?
+        document.documentElement.classList.remove('menu-open') : null;
+
+        const goTo = targetElement.closest('[data-goto]').dataset.goto;
+        const goToElement = document.querySelector(goTo);
+        const headerHeight = document.querySelector('.header').offsetHeight;
+        
+        if (goToElement) {
+            window.scrollTo({
+                top: goToElement.offsetTop - (headerHeight - 40),
+                behavior: "smooth"
+            })
+        }
+        e.preventDefault();
+    }
 }
